b2e541070d04e91aba6420b7576e81a48de73571
vincent edeh <vincentedeh42@gmail.com>
2023-11-13 22:10:10 +0100

    debug: comment out resend initialization

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from '@/components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST( request: Request,
     { params }: { params: { chatId: string } }) {
